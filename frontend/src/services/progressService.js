d223af680a19aa19328ebd54bc4e3289bff57037
kansari1904 <kansari1904@gmai.com>
2026-05-12 15:10:03 +0530

    fix vercel build issue

--- frontend/src/services/progressService.js
+++ frontend/src/services/progressService.js
@@ -1,4 +1,4 @@
-import axiosInstance from '../utils/axiosInstance';
+import axiosInstance  from '../utils/axiosInstance';
 import { API_PATHS } from '../utils/apiPaths';
 
 const getDashboardData = async () => {
